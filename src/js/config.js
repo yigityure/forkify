995e41b1a90d72f45392d63b3f4c66078ac8606b
yigityure <yigityure@gmail.com>
2021-11-23 20:42:54 +0200

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,9 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+
+export const TIMEOUT_SEC = 10;
+
+export const RES_PER_PAGE = 10;
+
+export const KEY = '883ae9af-a1b9-4df9-8bbb-131cda457aad';
+
+export const MODAL_CLOSE_SEC = 2.5;
